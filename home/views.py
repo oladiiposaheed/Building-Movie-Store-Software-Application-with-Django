3837e0cd51737254eabf4ca246eea8d0da64c698
Saheed <oladiiposaheed@gmail.com>
2025-08-05 12:10:33 +0100

    Add about page with corresponding view function and about template and update URL patterns for navigation

--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+#  Defining about function
+def about(request):
+    return render(request, 'home/about.html')
